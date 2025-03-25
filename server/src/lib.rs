c2a537ac2c3588470c27c63b535df66320f8bc77
Malted <malted@malted.dev>
2025-03-25 13:02:40 -0400

    Scrape airtable for ships

---
+++ server/src/lib.rs
@@ -0,0 +1,90 @@
+use time::{macros::format_description, Date};
+
+#[derive(Debug)]
+pub struct Ship {
+    pub id: String,
+    pub heard_through: Option<String>,
+    pub github_username: Option<String>,
+    pub country: Option<String>,
+    pub hours: Option<f64>,
+    pub screenshot_url: Option<String>,
+    pub code_url: Option<String>,
+    pub demo_url: Option<String>,
+    pub description: Option<String>,
+    pub approved_at: Option<Date>,
+}
+impl Ship {
+    pub async fn fetch_unified_page(
+        cursor: &Option<String>,
+    ) -> Option<(Vec<Self>, Option<String>)> {
+        let cursor_url_param = match cursor {
+            Some(c) => &format!("&offset={c}"),
+            None => "",
+        };
+        let url = format!("https://api.airtable.com/v0/app3A5kJwYqxMLOgh/Approved%20Projects?view=Ben - All{cursor_url_param}");
+
+        let res = reqwest::Client::new()
+            .get(url)
+            .bearer_auth(std::env::var("AIRTABLE_TOKEN").expect("an airtable PAT"))
+            .send()
+            .await
+            .ok()?
+            .json::<serde_json::Value>()
+            .await
+            .ok()?;
+
+        let cursor = res
+            .get("offset")
+            .map(|a| a.as_str().map(|b| b.to_string()))
+            .flatten();
+        let records = res.get("records").map(|r| r.as_array()).flatten()?;
+
+        let format = format_description!("[year]-[month]-[day]");
+
+        Some((
+            records
+                .iter()
+                .map(|t| {
+                    let f = t
+                        .get("fields")
+                        .map(|f| f.as_object())
+                        .flatten()
+                        .expect("arstrs");
+
+                    Ship {
+                        id: Self::field(t.get("id")).expect("a record id"),
+                        heard_through: Self::field(f.get("How did you hear about this?")),
+                        github_username: Self::field(f.get("GitHub Username")),
+                        country: t
+                            .pointer("/fields/Hack Clubber–Geocoded Country/0")
+                            .map(|a| a.as_str().map(|b| b.to_string()))
+                            .flatten(),
+                        hours: f.get("Hours Spent").map(|a| a.as_f64()).flatten(),
+                        screenshot_url: t
+                            .pointer("/fields/Screenshot/0/url")
+                            .and_then(|v| v.as_str())
+                            .map(|s| s.to_string()),
+                        code_url: Self::field(f.get("Code URL")),
+                        demo_url: Self::field(f.get("Playable URL")),
+                        description: Self::field(f.get("Description")),
+                        approved_at: f
+                            .get("Approved At")
+                            .map(|a| a.as_str().map(|b| Date::parse(b, &format).ok()))
+                            .flatten()
+                            .flatten(),
+                    }
+                })
+                .collect::<Vec<Ship>>(),
+            cursor,
+        ))
+    }
+
+    fn field(v: Option<&serde_json::Value>) -> Option<String> {
+        v.map(|s| {
+            s.as_str().map(
+                |a| a.to_string().replace('\0', ""), // Somebody's description had a null byte 😭
+            )
+        })
+        .flatten()
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,9 @@
 use actix_cors::Cors;
-use actix_web::{get, middleware, post, web, App, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{get, middleware, web, App, HttpServer, Responder, Result};
+use deadpool_postgres::{Config, GenericClient, ManagerConfig, RecyclingMethod};
 use serde::{Deserialize, Serialize};
-use std::{error::Error, io, process};
+use server::Ship;
+use std::error::Error;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct SanitisedShip {
@@ -38,22 +40,113 @@ fn get_ships() -> Result<Vec<SanitisedShip>, Box<dyn Error>> {
         .collect())
 }
 
-#[get("/ships")]
-async fn ships() -> Result<impl Responder> {
+#[get("/")]
+async fn index() -> Result<impl Responder> {
     Ok(web::Json(get_ships()?))
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    //#region DB setup
+    let mut db_cfg = Config::new();
+    db_cfg.url = Some(std::env::var("DB_URL").expect("a Postgres URL").to_string());
+    db_cfg.manager = Some(ManagerConfig {
+        recycling_method: RecyclingMethod::Fast,
+    });
+    let db_pool = db_cfg
+        .create_pool(
+            Some(deadpool_postgres::Runtime::Tokio1),
+            tokio_postgres::NoTls,
+        )
+        .unwrap();
+
+    db_pool
+        .get()
+        .await
+        .unwrap()
+        .batch_execute(
+            "CREATE TABLE IF NOT EXISTS ship (
+id TEXT PRIMARY KEY,
+heard_through TEXT,
+github_username TEXT,
+country TEXT,
+hours DOUBLE PRECISION,
+screenshot_url TEXT,
+code_url TEXT,
+demo_url TEXT,
+description TEXT,
+approved_at DATE
+);",
+        )
+        .await
+        .unwrap();
+
+    let insert_ship_stmt = db_pool
+        .get()
+        .await?
+        .prepare_cached("
+            INSERT INTO ship (id, heard_through, github_username, country, hours, screenshot_url, code_url, demo_url, description, approved_at)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+            ON CONFLICT (id) DO UPDATE SET
+                heard_through = $2,
+                github_username = $3,
+                country = $4,
+                hours = $5,
+                screenshot_url = $6,
+                code_url = $7,
+                demo_url = $8,
+                description = $9,
+                approved_at = $10;")
+        .await?;
+    //#endregion
+
+    let pool_item = db_pool.get().await?;
+    tokio::task::spawn(async move {
+        let mut cursor = None::<String>;
+
+        loop {
+            if let Some((ships, new_cursor)) = Ship::fetch_unified_page(&cursor).await {
+                cursor = new_cursor;
+                for ship in ships.iter() {
+                    pool_item
+                        .query(
+                            &insert_ship_stmt,
+                            &[
+                                &ship.id,
+                                &ship.heard_through,
+                                &ship.github_username,
+                                &ship.country,
+                                &ship.hours,
+                                &ship.screenshot_url,
+                                &ship.code_url,
+                                &ship.demo_url,
+                                &ship.description,
+                                &ship.approved_at,
+                            ],
+                        )
+                        .await
+                        .expect("the query to execute");
+                }
+            }
+            println!("Inserted");
+
+            tokio::time::sleep(tokio::time::Duration::from_millis(1_000)).await;
+        }
+    });
+
     HttpServer::new(|| {
         let cors = Cors::permissive();
 
         App::new()
             .wrap(middleware::Compress::default())
             .wrap(cors)
-            .service(ships)
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
-    .await
+    .await?;
+
+    Ok(())
 }
